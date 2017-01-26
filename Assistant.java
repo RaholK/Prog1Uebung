af0001c2458545b8ccfea5387735ea8f8fe1dd8a
RaholK <rahol97@live.de>
2017-01-26 16:53:04 +0100

    Update Assistant.java

--- Assistant.java
+++ Assistant.java
@@ -9,30 +9,51 @@ public class Assistant extends Staff {
 	public Assistant(String name, String prenom, String adress, String room, String telephoneNumber, Professor supervisor) {
 		super(name, prenom, adress, room, telephoneNumber);
 		this.supervisor = supervisor;
+		int count = 0;
+		for(int i = 0; i<supervisor.assistants.length; i++) {
+			if(supervisor.assistants[i]==null) {
+				count++;
+			}
+		}
+		if(count==0) {
+			System.exit(0);
+		}
 		for(int i = 0; i<supervisor.assistants.length; i++) {
 			if(supervisor.assistants[i] == null) {
 				supervisor.assistants[i] = this;
+				
 				break;
 			}
-			System.exit(0);
 		}
 		
 	}
 	public void resign() { 
-		for(int i = 0; i<supervisor.assistants.length; i++) {
-			if(supervisor.assistants[i] == this) {
-				supervisor.assistants[i] = null;
+		if(supervisor!=null) {
+			for(int i = 0; i<supervisor.assistants.length; i++) {
+				if(supervisor.assistants[i] == this) {
+					supervisor.assistants[i] = null;
+					supervisor = null;
+					break;
+				}
+				
 			}
 		}
+		
 	}
 	
 	public boolean employed() {
-		for(int i = 0; i<supervisor.assistants.length; i++) {
+		if(supervisor!=null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		/*for(int i = 0; i<supervisor.assistants.length; i++) {
 			if(supervisor.assistants[i] == this) {
 				return true;
 			}
 		}
-		return false;
+		return false; */
 			
 	}
 	
